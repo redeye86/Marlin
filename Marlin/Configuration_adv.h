4736c07f60c0e6e89d1a2be18bdd92ddb3f05b64
Florian Schulte <mail@florian-schulte.net>
2020-01-11 21:04:48 +0100

    fixed bltouch delay #up

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -446,7 +446,7 @@
    * V3.1: Force a probe with unknown mode into selected mode at Marlin startup ( = Probe EEPROM write )
    * To preserve the life of the probe, use this once then turn it off and re-flash.
    */
-  //#define BLTOUCH_FORCE_MODE_SET
+  #define BLTOUCH_FORCE_MODE_SET
 
   // Safety: Enable voltage mode settings in the LCD menu.
   //#define BLTOUCH_LCD_VOLTAGE_MENU
